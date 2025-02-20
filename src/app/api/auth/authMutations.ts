98dc837bf8b6c9d3ffdb5ac2e28e50803ced45ca
OussemaJridi <oussemajridi78@gmail.com>
2025-02-20 21:03:36 +0100

    delete old files

--- src/app/api/auth/authMutations.ts
+++
@@ -1,7 +0,0 @@
-import { apiRequest } from "@/lib/apiRequest";
-import { useMutation } from "@tanstack/react-query";
-const loginFn = async (userData: { email: string; password: string }) =>
-	apiRequest.post(import.meta.env.VITE_API_URI + "/login", userData);
-export const useLoginMutation = () => {
-	return useMutation({ mutationFn: loginFn });
-};
