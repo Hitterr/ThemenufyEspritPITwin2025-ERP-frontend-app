4637b2dce0aa2f7cff071907d6e2b7fc2562919d
Oussema Abdellaoui <117827078+Oussemabdellaoui@users.noreply.github.com>
2025-02-22 19:43:21 +0100

    ForgotPassword & LogOut
    Forgot password with simulated verification code and log out button implemented by alerts

--- src/jsx/pages/auth/AuthRouting.jsx
+++ src/jsx/pages/auth/AuthRouting.jsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./login/Login";
+import ForgotPassword from "./reset-password/ForgotPassword";
+import Logout from './logout/Logout';
 export default function AuthRouting() {
 	return (
 		<Routes>
 			<Route path="login" element={<Login />}></Route>
+			<Route path="forgotPassword" element={<ForgotPassword />}></Route>
+			<Route path="logOut" element={<Logout />}></Route>
 		</Routes>
 	);
 }
