29afdd97622f4759c7f15ad679211cac72c57695
OussemaJridi <oussemajridi78@gmail.com>
2025-02-26 09:30:07 +0100

    Add Forgot Password Feature
    commit 4637b2dce0aa2f7cff071907d6e2b7fc2562919d
    Author: Oussema Abdellaoui <117827078+Oussemabdellaoui@users.noreply.github.com>
    Date:   Sat Feb 22 19:43:21 2025 +0100
    
        ForgotPassword & LogOut
    
        Forgot password with simulated verification code and log out button implemented by alerts

--- src/jsx/pages/auth/AuthRouting.jsx
+++ src/jsx/pages/auth/AuthRouting.jsx
@@ -2,19 +2,19 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./login/Login";
 import { authStore } from "../../store/authStore";
 import { useEffect } from "react";
+import ForgotPassword from "./reset-password/ForgotPassword";
 export default function AuthRouting() {
 	const { currentUser } = authStore();
 	const navigate = useNavigate();
 	useEffect(() => {
 		if (currentUser == null) {
 			navigate("/login");
-		} else {
-			navigate("/");
 		}
-	}, [currentUser]);
+	}, [currentUser, navigate]);
 	return (
 		<Routes>
 			<Route path="login" element={<Login />}></Route>
+			<Route path="forgotPassword" element={<ForgotPassword />}></Route>
 		</Routes>
 	);
 }
