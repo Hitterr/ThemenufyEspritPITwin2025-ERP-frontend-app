51cdbae37bbc289f4e9c5fdebcd6c6c81344b9df
OussemaJridi <oussemajridi78@gmail.com>
2025-03-05 17:45:04 +0100

    Edit UserInfo Profile is Done

--- src/jsx/pages/auth/profile/components/EditForm.jsx
+++ src/jsx/pages/auth/profile/components/EditForm.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Col, Row, Stack } from "react-bootstrap";
