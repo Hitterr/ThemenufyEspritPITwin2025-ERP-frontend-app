1cecc099fe185780eb320a43f907609e0c501bf9
OussemaJridi <oussemajridi78@gmail.com>
2025-03-12 00:12:01 +0100

    update login design

--- src/jsx/pages/auth/login/FacebookAuth.jsx
+++ src/jsx/pages/auth/login/FacebookAuth.jsx
@@ -4,25 +4,25 @@ import { FaFacebook } from "react-icons/fa";
 import { authStore } from "../../../store/authStore";
 import { useNavigate } from "react-router-dom";
 const FacebookAuth = () => {
-  const { loginFacebook } = authStore();
-  const navigate = useNavigate();
-  const responseFacebook = async (response) => {
-    try {
-      await loginFacebook(response.accessToken);
-      navigate("/");
-    } catch (error) {}
-  };
-  return (
-    <FacebookLogin
-      appId={clientId}
-      autoLoad={true}
-      callback={responseFacebook}
-      fields="name,email,picture"
-      cssClass="btn btn-secondary w-100  m-0"
-      buttonStyle={{ height: "40px", textAlign: "center", padding: 0 }}
-      textButton=""
-      icon={<FaFacebook size={20} className="mx-2" />}
-    />
-  );
+	const { loginFacebook } = authStore();
+	const navigate = useNavigate();
+	const responseFacebook = async (response) => {
+		try {
+			await loginFacebook(response.accessToken);
+			navigate("/");
+		} catch (error) {}
+	};
+	return (
+		<FacebookLogin
+			appId={clientId}
+			autoLoad={true}
+			callback={responseFacebook}
+			fields="name,email,picture"
+			cssClass="btn btn-outline-secondary w-100 my-1  m-0"
+			buttonStyle={{ height: "40px", textAlign: "center", padding: 0 }}
+			textButton=""
+			icon={<FaFacebook size={20} className="mx-2" />}
+		/>
+	);
 };
 export default FacebookAuth;
--- src/jsx/pages/auth/login/GoogleAuth.jsx
+++ src/jsx/pages/auth/login/GoogleAuth.jsx
@@ -1,34 +1,42 @@
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID; // Replace with your Google OAuth client ID
-import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
 import { authStore } from "../../../store/authStore";
 import { Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import { useRef } from "react";
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID; // Replace with your Google OAuth client ID
 const GoogleAuth = () => {
-  const { googleLogin } = authStore();
-  const navigate = useNavigate();
-  return (
-    <Col>
-      <GoogleOAuthProvider clientId={clientId}>
-        <GoogleLogin
-          onSuccess={async (credentialResponse) => {
-            try {
-              console.log("📢 [GoogleAuth.jsx:18]", credentialResponse);
-              await googleLogin(credentialResponse.credential);
-              navigate("/");
-            } catch (error) {}
-          }}
-          size="large"
-          shape="rectangular"
-          type="standard"
-          text=" sign_in_with_google"
-          theme="outline"
-          onError={() => {
-            console.log("Login Failed");
-          }}
-        />
-        ;
-      </GoogleOAuthProvider>
-    </Col>
-  );
+	const { googleLogin } = authStore();
+	const ref = useRef();
+	const navigate = useNavigate();
+	const login = useGoogleLogin({
+		onSuccess: async (tokenResponse) => {
+			try {
+				console.log("📢 [GoogleAuth.jsx:18]", tokenResponse);
+				await googleLogin(tokenResponse.access_token);
+				navigate("/");
+			} catch (error) {
+				console.error("Google login error:", error);
+			}
+		},
+		onError: () => {
+			console.log("Login Failed");
+		},
+	});
+	return (
+		<Col>
+			<GoogleOAuthProvider clientId={clientId}>
+				<button
+					className="btn btn-outline-secondary  w-100 text-center p-0 my-1"
+					style={{ height: "40px" }}
+					onClick={() => {
+						return login();
+					}}
+				>
+					<FaGoogle size={20} />
+				</button>
+			</GoogleOAuthProvider>
+		</Col>
+	);
 };
 export default GoogleAuth;
