44de7a38cbd71850be91314edfba4466ffab23b6
Oussema Abdellaoui <abdellaoui.oussema@esprit.tn>
2025-04-27 22:27:42 +0200

    Update InvoicesPagination.jsx

--- src/jsx/pages/invoice/components/InvoicesPagination.jsx
+++ src/jsx/pages/invoice/components/InvoicesPagination.jsx
@@ -1,33 +1,22 @@
-import React from "react";
-import { Pagination } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const InvoicesPagination = ({ currentPage, totalPages, onPageChange }) => {
-  const pages = [];
-
-  for (let i = 1; i <= totalPages; i++) {
-    pages.push(
-      <Pagination.Item
-        key={i}
-        active={i === currentPage}
-        onClick={() => onPageChange(i)}
-      >
-        {i}
-      </Pagination.Item>
-    );
-  }
-
   return (
-    <Pagination className="justify-content-center mt-3">
-      <Pagination.Prev
-        disabled={currentPage === 1}
+    <div className="d-flex justify-content-between mt-3">
+      <Button
         onClick={() => onPageChange(currentPage - 1)}
-      />
-      {pages}
-      <Pagination.Next
-        disabled={currentPage === totalPages}
+        disabled={currentPage === 1}
+      >
+        Prev
+      </Button>
+      <span>Page {currentPage} of {totalPages}</span>
+      <Button
         onClick={() => onPageChange(currentPage + 1)}
-      />
-    </Pagination>
+        disabled={currentPage === totalPages}
+      >
+        Next
+      </Button>
+    </div>
   );
 };
 
